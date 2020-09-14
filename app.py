23983cd03666258510975b8c91dd26b2a6c281d2
Marina8989 <marina.mill209@gmail.com>
2020-09-14 14:29:17 -0700

    python simple game

---
+++ app.py
@@ -0,0 +1,9 @@
+print('Welcome to my first game!')
+
+name = input('What is you name? ')
+age = int(input('What is your age? '))
+if age >= 18:
+    print('You are old enough to play!')
+else:
+     print('Sorry, you are not old enough to play!')   
+name
