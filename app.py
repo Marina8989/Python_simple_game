c1470904f9fcc1be292d1754e141e477ea9bc8dd
Marina8989 <marina.mill209@gmail.com>
2020-09-14 20:07:02 -0700

    python simple game

--- app.py
+++ app.py
@@ -4,6 +4,16 @@ name = input('What is you name? ')
 age = int(input('What is your age? '))
 if age >= 18:
     print('You are old enough to play!')
+    play = input('Do you want to play? ').lower()
+    if play == 'yes':
+        print('You are starting with 10 health')
+        print('Let\'s play!')
+        choice = input('First choice... Left or Right? ').lower()
+        if choice == 'left':
+            
+    else:
+        print('Game over')    
+
 else:
      print('Sorry, you are not old enough to play!')   
-name
+
